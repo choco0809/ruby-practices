e402efb29c4ca19dc7cd023dfd7fa307984fcf0e
choco <superbeaver.180809@gmail.com>
2022-03-08 23:43:35 +0900

    [update]リファクタリング実施

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,33 +1,28 @@
 # frozen_string_literal: true
 
-score = ARGV[0]
-score_array = score.to_s.split(',')
-score_array_num = []
-score_array.each do |s|
-  if s[0] == 'X'
-    score_array_num.push(10)
-    score_array_num.push(0)
-  else
-    score_array_num.push(s.to_i)
-  end
-end
-frames = []
-score_array_num.each_slice(2) do |s|
-  frames.push(s)
-end
-total_score = 0
-frames.each_with_index do |f, index|
-  total_score =
-    if index + 1 >= 10
-      total_score + f.sum
-    elsif f[0] == 10 && frames[index + 1][0] == 10
-      total_score + frames[index + 1][0] + frames[index + 2][0] + 10
-    elsif f[0] == 10 && frames[index + 1][0] != 10
-      total_score + frames[index + 1].sum + 10
-    elsif f.sum == 10
-      total_score + frames[index + 1][0] + 10
+text_scores = ARGV[0].split(',')
+scores = text_scores.flat_map { |text_score| text_score == 'X' ? [10, 0] : text_score.to_i }
+frames = scores.each_slice(2).to_a
+total_score =
+  frames.each_with_index.sum do |frame, index|
+    strike = (frame[0] == 10)
+    spare = (!strike && frame.sum == 10)
+    last_frame = (index + 1 >= 10)
+    next_frame = (frames[index + 1])
+    after_next_frame = (frames[index + 2])
+    if last_frame
+      frame.sum
+    elsif strike
+      double = (next_frame[0] == 10)
+      if double
+        after_next_frame[0] + 20
+      else
+        next_frame.sum + 10
+      end
+    elsif spare
+      next_frame[0] + 10
     else
-      total_score + f.sum
+      frame.sum
     end
-end
+  end
 puts total_score
