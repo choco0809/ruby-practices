c410eb6fdd753cded66648057c73acc11e8b8e17
choco <superbeaver.180809@gmail.com>
2022-04-09 20:19:30 +0900

    [update]リファクタリング実施

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,14 +4,61 @@ require 'optparse'
 
 SPACE_STRING = '0x0c|0x0a|0x0d|0x09|0x0b|0xa0|0x85'
 
-# 容量（バイト）取得
-def get_size(string)
-  string.size
+def main
+  # オプションの初期化
+  options = { l: false }
+  opt = OptionParser.new
+  opt.on('-l', '入力ファイルの行数を標準出力に出力します。改行コードの数を行数とします。') { options[:l] = true }
+  argument_flg = opt.parse(ARGV).first.nil? ? false : true
+  input_contents = argument_flg ? opt.parse(ARGV) : $stdin.read
+  # total_summary = []
+  if argument_flg
+    process_has_argument_flg(input_contents, **options)
+  else
+    # 標準入力の場合
+    process_without_argument_flg(input_contents, **options)
+  end
 end
 
-# 改行コードの数を取得
-def get_line_count(string)
-  string.scan("\n").count
+# 引数ありの処理
+def process_has_argument_flg(input_contents, options)
+  total_summary = []
+  input_contents.each do |input_content|
+    if FileTest.directory? input_content
+      puts 'wc: wc_test: read: Is a directory'
+      files_list = { name: nil, size: 0, word_count: 0, line_count: 0 }
+      total_summary.push(files_list)
+    else
+      full_path = File.expand_path(input_content)
+      stat_file = File::Stat.new(full_path)
+      read_contents = IO.readlines(full_path).join
+      files_list = create_files_list(input_content, stat_file, read_contents, read_contents)
+      total_summary.push(files_list)
+      puts_wc(files_list, **options)
+    end
+  end
+  total_summary.count > 1 ? puts_total_summary(total_summary, **options) : nil
+end
+
+# 引数なしの処理
+def process_without_argument_flg(input_contents, options)
+  files_list = create_files_list(nil, input_contents, input_contents, input_contents)
+  puts_wc(files_list, **options)
+end
+
+# files_listを作成する
+def create_files_list(name, size, word_count, line_count)
+  {
+    name: name,
+    size: get_size(size),
+    word_count: get_word_count(word_count),
+    line_count: get_line_count(line_count)
+  }
+end
+
+# 容量（バイト）取得
+def get_size(string)
+  string.is_a?(String) ? string.bytesize : string.size
 end
 
 # 単語の数取を得
@@ -37,6 +84,11 @@ def delete_consecutive_0x20(string)
   else
     string
   end
+end
+
+# 改行コードの数を取得
+def get_line_count(string)
+  string.scan("\n").count
 end
 
 # wcコマンドの出力を整形
@@ -70,42 +122,4 @@ def puts_total_summary(total_summary, options)
   puts puts_wc_string
 end
 
-# files_listを作成する
-def create_files_list(name, size, word_count, line_count)
-  {
-    name: name,
-    size: get_size(size),
-    word_count: get_word_count(word_count),
-    line_count: get_line_count(line_count)
-  }
-end
-
-# オプションの初期化
-options = { l: false }
-opt = OptionParser.new
-opt.on('-l', '入力ファイルの行数を標準出力に出力します。改行コードの数を行数とします。') { options[:l] = true }
-argument_flg = opt.parse(ARGV).first.nil? ? false : true
-input_contents = argument_flg ? opt.parse(ARGV) : $stdin.read
-total_summary = []
-if argument_flg
-  # ディレクトリか判断
-  input_contents.each do |input_content|
-    if FileTest.directory? input_content
-      puts 'wc: wc_test: read: Is a directory'
-      files_list = { name: nil, size: 0, word_count: 0, line_count: 0 }
-      total_summary.push(files_list)
-    else
-      full_path = File.expand_path(input_content)
-      stat_file = File::Stat.new(full_path)
-      read_contents = IO.readlines(full_path).join
-      files_list = create_files_list(input_content, stat_file, read_contents, read_contents)
-      total_summary.push(files_list)
-      puts_wc(files_list, **options)
-    end
-  end
-  total_summary.count > 1 ? puts_total_summary(total_summary, **options) : nil
-else
-  # 標準入力の場合
-  files_list = create_files_list(nil, input_contents, input_contents, input_contents)
-  puts_wc(files_list, **options)
-end
+main
