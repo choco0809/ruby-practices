07c5d5c7d0aeb0fe6271c970510e6346456e2f71
choco <superbeaver.180809@gmail.com>
2022-06-04 08:26:07 +0900

    [update] Frameクラス作成

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,1 +1,25 @@
+# frozen_string_literal: true
 
+class Frame
+  def initialize(frame, next_frame, after_next_frame)
+    @frame = frame
+    @next_frame = next_frame
+    @after_next_frame = after_next_frame
+  end
+
+  def score(index)
+    strike = (@frame[0] == 10)
+    spare = (!strike && @frame.sum == 10)
+    last_frame = (@frame if index > 8)
+    if last_frame
+      @frame.sum + @next_frame.sum + @after_next_frame.sum
+    elsif strike
+      double = (@next_frame[0] == 10)
+      double ? @after_next_frame[0] + 20 : @next_frame.sum + 10
+    elsif spare
+      @next_frame[0] + 10
+    else
+      @frame.sum
+    end
+  end
+end
