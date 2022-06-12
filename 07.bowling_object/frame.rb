ce202b8849a4409a3a0c50b7489f3f946fef76ad
choco <superbeaver.180809@gmail.com>
2022-06-12 09:58:12 +0900

    [update] 変数名を適切な名前に変更

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -9,23 +9,23 @@ class Frame
 
   def score(index)
     if index > 8
-      last_frame
+      score_for_last_frame
     elsif @frame.sum == 10
-      spare + strike
+      score_for_spare + score_for_strike
     else
       @frame.sum
     end
   end
 
-  def last_frame
+  def score_for_last_frame
     [@frame, @next_frame, @after_next_frame].flatten.sum
   end
 
-  def spare
+  def score_for_spare
     @frame[0] != 10 ? @next_frame[0] + 10 : 0
   end
 
-  def strike
+  def score_for_strike
     if @frame[0] == 10
       @next_frame[0] == 10 ? @after_next_frame[0] + 20 : @next_frame.sum + 10
     else
